a64bbe3b987cf34d18896ab1832f1bcf7794adb6
SomeshSawant <sawantsomesh134@gmail.com>
2025-11-09 03:43:24 +0530

    updated struct of pages

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANzy2Fl54zbSLMMQtRP6uKEEsqMpFxk_c",
+  authDomain: "campus-connect-27e8e.firebaseapp.com",
+  projectId: "campus-connect-27e8e",
+  storageBucket: "campus-connect-27e8e.firebasestorage.app",
+  messagingSenderId: "1093775901397",
+  appId: "1:1093775901397:web:943f9d7425a417d5a1d85c",
+  measurementId: "G-VJYLCCP55E"
+};
--- script.js
+++ script.js
@@ -28,17 +28,6 @@ document.addEventListener('click', function(e) {
 });
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyANzy2Fl54zbSLMMQtRP6uKEEsqMpFxk_c",
-  authDomain: "campus-connect-27e8e.firebaseapp.com",
-  projectId: "campus-connect-27e8e",
-  storageBucket: "campus-connect-27e8e.firebasestorage.app",
-  messagingSenderId: "1093775901397",
-  appId: "1:1093775901397:web:943f9d7425a417d5a1d85c",
-  measurementId: "G-VJYLCCP55E"
-};
-
 // Initialize Firebase app
 const app = firebase.initializeApp(firebaseConfig);
 
