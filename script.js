f469e18f1a22251d4f0c7e7cdfea9632cd8a6391
SomeshSawant <sawantsomesh134@gmail.com>
2025-11-08 17:39:31 +0530

    Save local changes before pulling

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ document.querySelectorAll('.switch-auth a').forEach(link => {
     targetForm.scrollIntoView({ behavior: 'smooth' });
   });
 });
+// Firebase Configuration and Initialization
+  // Import the functions you need from the SDKs you need
+  // import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  // import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+  // // TODO: Add SDKs for Firebase products that you want to use
+  // // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // // Your web app's Firebase configuration
+  // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyDMWgsbBQw0I7h_Hnx1ZFkel7hBlWLbGC4",
+  //   authDomain: "campus-connect-5674c.firebaseapp.com",
+  //   projectId: "campus-connect-5674c",
+  //   storageBucket: "campus-connect-5674c.firebasestorage.app",
+  //   messagingSenderId: "1065784810799",
+  //   appId: "1:1065784810799:web:f88316aa0160b1092495da",
+  //   measurementId: "G-HHHZR9R7CJ"
+  // };
+
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
